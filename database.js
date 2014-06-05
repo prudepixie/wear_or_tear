9d675d24cf869bc83a44414283ffbeaff0fa3a5a
Wendy Hsu <wndyhsu@gmail.com>
2014-06-05 16:35:14 -0500

    added some mock photos

--- database.js
+++ database.js
@@ -1,3 +1,11 @@
 var Datastore = require ('nedb'),
 fs = require ('fs');
 
+var photos = new Datastore ({filename: _dirname + '/data/photos', autoload: true}),
+    users = new Datastore ({filename: _dirname + '/data/users', autoload: true});
+
+photos.ensureIndex ({fieldName: 'name', unique: true});
+users.ensureIndex ({fieldName: 'ip', unique: true});
+
+
+var photos_on_disk = fs.readdirSync(_dirname + '/public/photos');
